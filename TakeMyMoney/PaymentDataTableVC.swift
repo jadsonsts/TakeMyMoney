5302c6a78696c3b45e4a3e094d09e2c7cc2191b1
jadsonsts <jadson.s.carneiro@gmail.com>
2022-03-31 23:45:38 +1300

    refine layout, send data to month/year label, start word on height for row

--- TakeMyMoney/PaymentDataTableVC.swift
+++ TakeMyMoney/PaymentDataTableVC.swift
@@ -29,6 +29,17 @@ class PaymentDataTableVC: UITableViewController {
     var bankPayment: BankTransferPayment!
     var cardPayment: CardPayment?
     var payPalPayment: PayPalPayment?
+    var isEditingExpirationDate: Bool = false {
+        didSet {
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    
+    var cardRowSection = (row: 1, section: 2)
+    var expirationDateRowSection = (row: 1, section: 3)
+    var paypalRowSection = (row: 1, section: 4)
+    var bankTransferRowSection = (row: 1, section: 5)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,15 +47,73 @@ class PaymentDataTableVC: UITableViewController {
         paymentTypeCollectionView.dataSource = self
         paymentTypeCollectionView.delegate = self
         paymentTypeCollectionView.allowsSelection = true
-
         
         proceedPaymentButton.layer.cornerRadius = 15
-//        proceedPaymentButton.layer.borderWidth = 1
         
 //        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
 
-    }
 
+    }
+    
+//    func updateView() {
+//        if let cardPayment = cardPayment {
+//            cardNumberTextField.text = String(cardPayment.cardNumber)
+//            let dateFormatter = DateFormatter()
+//            dateFormatter.dateStyle = .short
+//            expirationDateTextField.text = dateFormatter.string(from: cardPayment.expDate)
+//            CVVTextField.text = String(cardPayment.CVV)
+//            cardHolderNameTextField.text = cardPayment.cardHolderName
+//        } else if let payPalPayment = payPalPayment {
+//            payPalLoginTextField.text = payPalPayment.login
+//            payPalPasswordTextField.text = payPalPayment.password
+//        }
+//    }
+    
+    func updateExpireDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        expirationDateTextField.text = String(dateFormatter.string(from: expireDatePicker.date).dropFirst(3))
+        
+        disableProceedButton()
+    }
+    
+    func disableProceedButton() {
+        
+    }
+    
+    
+    @IBAction func proceedButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateExpireDate()
+        
+    }
+    
+    //MARK: - Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch (indexPath.row, indexPath.section) {
+        case (cardRowSection.row, cardRowSection.section):
+            return 280
+        case (expirationDateRowSection.row, expirationDateRowSection.section):
+            return expireDatePicker.frame.height
+            
+        case (paypalRowSection.row, paypalRowSection.section):
+            return 130
+            
+        case (bankTransferRowSection.row, bankTransferRowSection.section):
+            return 52
+            
+        default:
+            return 280.0
+        }
+    }
+    
+    
+    
+    
 }
 
 extension PaymentDataTableVC: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -56,10 +125,8 @@ extension PaymentDataTableVC: UICollectionViewDelegate, UICollectionViewDataSour
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PropertyKeys.paymentTypesId, for: indexPath) as? PaymentTypeCollectionViewCell {
             let text = PaymentMethods.all[indexPath.row]
             cell.updateView(paymentType: text)
-            
-            if cell.isSelected {
-                cell.backgroundColor = .blue
-            }
+            cell.layer.cornerRadius = 15
+            cell.layer.borderWidth = 0.5
             return cell
         }
         return PaymentTypeCollectionViewCell()
